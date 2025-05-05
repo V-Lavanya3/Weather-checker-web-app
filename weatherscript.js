7543fca9b5e807e043702fa12d973ebc7cd81caa
V-Lavanya3 <lavanyatrichy484@gmail.com>
2025-05-05 15:12:24 +0530

    Updated JavaScript logic

--- weatherscript.js
+++ weatherscript.js
@@ -6,7 +6,6 @@ function weatherInfo() {
   const city2 = document.getElementById('city2').value;
   const weatherContainer = document.getElementById('weatherContainer');
   weatherContainer.innerHTML = '';
-
   if (city1) fetchWeather(city1);
   if (city2) fetchWeather(city2);
 }
@@ -14,8 +13,8 @@ function weatherInfo() {
 function fetchWeather(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1cde13fa70d4e39f70fb61c72a41a72f&units=metric`)
     .then(res => {
-      if (!res.ok) throw new Error("City not found");
-      return res.json();
+     if (!res.ok) throw new Error("City not found");
+     return res.json();
     })
     .then(data => displayWeather(data))
     .catch(err => alert(err.message));
@@ -25,19 +24,17 @@ function displayWeather(data) {
   const container = document.getElementById('weatherContainer');
   const card = document.createElement('div');
   card.className = 'col-md-6 mb-4';
-
   card.innerHTML = `
     <div class="card shadow-sm">
-      <div class="card-body">
+    <div class="card-body">
         <h5 class="card-title">${data.name}</h5>
         <p class="card-text">🌡 Temp: ${data.main.temp} °C</p>
         <p class="card-text">💧 Humidity: ${data.main.humidity}%</p>
         <p class="card-text">🌤 Condition: ${data.weather[0].main}</p>
         <button class="btn btn-outline-success btn-sm" onclick="saveCity('${data.name}')">Add to favourites</button>
-      </div>
+    </div>
     </div>
   `;
-
   container.appendChild(card);
 }
 
@@ -53,14 +50,13 @@ function saveCity(city) {
 function loadFavorites() {
   const favoritesDiv = document.getElementById('favorites');
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-
   favoritesDiv.innerHTML = '';
   favorites.forEach(city => {
     const btn = document.createElement('button');
     btn.className = 'btn btn-sm btn-outline-primary';
     btn.innerText = city;
     btn.onclick = () => {
-      document.getElementById('city1').value = city;
+    document.getElementById('city1').value = city;
       document.getElementById('city2').value = '';
       weatherInfo();
     };
